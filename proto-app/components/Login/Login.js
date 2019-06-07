c46afddd1ebfe2d0e0113b16a9f374778153c575
Weuller Jr. Souza <weuller9505@live.com>
2019-06-07 20:25:42 -0300

    KeyboardAvoidingView corrigido

--- proto-app/components/Login/Login.js
+++ proto-app/components/Login/Login.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Image, Text, KeyboardAvoidingView, } from 'react-native';
+import { StyleSheet, View, Image, Text, KeyboardAvoidingView } from 'react-native';
 import LoginForm from './LoginForm';
 
 export default class Login extends Component {
     render() {
-        return (   
-            <KeyboardAvoidingView behavior={'paddig'} style={styles.container}>
+        return (
+            <KeyboardAvoidingView  behavior={'padding'} style={styles.container}>
                 <View style={styles.logoContainer}>
                     <Image
                     style={styles.logo}
