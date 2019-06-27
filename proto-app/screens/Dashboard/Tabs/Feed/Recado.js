2171104c6556b47f9792fb869bfd13530659c1b4
Gabrielle inacio <jenniferp.i8@gmail.com>
2019-06-26 23:12:46 -0300

    Listar Atividades e Recado

--- proto-app/screens/Dashboard/Tabs/Feed/Recado.js
+++ proto-app/screens/Dashboard/Tabs/Feed/Recado.js
@@ -1,72 +1,125 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, TextInput} from 'react-native';
-import Recado from "./Recado"
+import { AppRegistry, StyleSheet, ActivityIndicator, ListView, Text, View, Alert, Platform, TouchableOpacity} from 'react-native';
+//import Recado from "./Recado";
 
-import {createAppContainer, createBottomTabNavigator} from 'react-navigation';
 
 import Timeline from 'react-native-timeline-listview';
-
-export default class Example extends Component {
-    constructor() {
-        super()
-        //this.onEventPress = this.onEventPress.bind(this)
-        this.data = [
-            {
-
-                title: 'Aluna não quis Almoçar/Gabrielle',
-                description: 'A Aluna não quis comer o lanche, alegava estar com dor de barriga ', backgroundColor:'#6ebcbc'
-
-            }
-        ],
-        this.state = {
-            selected: null
-        }
-
+export default class App extends React.Component   {
+ 
+    constructor(props) {
+      super(props);
+      this.state = {
+        isLoading: true
+      }
     }
-
-    // onEventPress(data){
-    // this.setState({selected: data})
-    //}
-
-    renderSelected() {
-      if (this.state.selected)
-          return <Text style={{
-                  marginTop: 10
-              }}>Selected event: {this.state.selected.title}
-              at {this.state.selected.time}</Text>
-  }
-
-
+     
+    GetItem (id_recado) {
+     
+    Alert.alert(id_recado);
+     
+    }
+     
+     
+    componentDidMount() {
+     
+      return fetch('https://coworkingsegunda.000webhostapp.com/consultaRecados.php')
+        .then((response) => response.json())
+        .then((responseJson) => {
+          let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+          this.setState({
+            isLoading: false,
+            dataSource: ds.cloneWithRows(responseJson),
+          }, function() {
+            // In this block you can do something with new state.
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+     
+    ListViewItemSeparator = () => {
+      return (
+        <View
+          style={{
+     
+            height: .5,
+            width: "100%",
+            backgroundColor: "#000",
+     
+          }}
+        />
+      );
+    }
+     
+     
     render() {
-      return (<View style={styles.container}>
-
-          <Timeline style={styles.list} data={this.data}
-               descriptionStyle={{
-                  color: 'gray'
-              }} separator={true} detailContainerStyle={{
-                marginBottom: 20,
-                paddingLeft: 5,
-                paddingRight: 5,
-                backgroundColor: "#def9ff",
-
-            }}
-              circleSize={-5}
-
-              />
-      </View>);
-  }
-}
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 20,
-      paddingTop:65,
-      backgroundColor:'white'
+      if (this.state.isLoading) {
+        return (
+          <View style={{flex: 1, paddingTop: 20}}>
+            <ActivityIndicator />
+          </View>
+        );
+      }
+     
+      return (
+     
+        <View style={styles.MainContainer}>
+     
+          <ListView
+     
+            dataSource={this.state.dataSource}
+     
+            renderSeparator= {this.ListViewItemSeparator}
+     
+            renderRow={(rowData) =>
+     
+           <View style={{flex:1, flexDirection: 'column'}} >
+     
+             <TouchableOpacity onPress={this.GetItem.bind(this, rowData.id_recado)} >
+           
+             <Text style={styles.textViewContainer} >{'Descrição = ' + rowData.descricao}</Text>
+     
+             <Text style={styles.textViewContainer} >{'Nome do Aluno = ' + rowData.nome_do_aluno}</Text>
+     
+             <Text style={styles.textViewContainer} >{'id_turma = ' + rowData.id_turma}</Text>
+     
+             <Text style={styles.textViewContainer} >{'id_responsavel = ' + rowData.id_responsavel}</Text>
+     
+             </TouchableOpacity>
+     
+           </View>
+     
+            }
+          />
+     
+        </View>
+      );
+    }
+    }
+     
+    const styles = StyleSheet.create({
+     
+    MainContainer :{
+     
+    // Setting up View inside content in Vertically center.
+    justifyContent: 'center',
+    flex:1,
+    paddingTop: (Platform.OS === 'ios') ? 20 : 0,
+    backgroundColor: '#fff',
+    padding: 5,
+     
     },
-    list: {
-      flex: 1,
-      marginTop:20,
-
-    },
-  });
+     
+    textViewContainer: {
+     
+     textAlignVertical:'center', 
+     padding:10,
+     fontSize: 20,
+     color: '#000',
+     
+    }
+     
+    });
+     
+    AppRegistry.registerComponent('App', () => App);
