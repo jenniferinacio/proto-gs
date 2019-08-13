a75bb4183784c647ec26b443e6c43664c466c4e0
Weuller Jr. Souza <weuller9505@live.com>
2019-08-13 15:58:52 -0300

    Estilo do login recuperado

--- proto-app/screens/Login.js
+++ proto-app/screens/Login.js
@@ -3,13 +3,13 @@ import { StyleSheet, Button, View, Image, Text, TextInput, TouchableOpacity, Key
 const MONITOR = 1;
 const PARENTE = 0;
 
-global.TYPE_USER = 0;   
+global.TYPE_USER = 0;
 global.EMAIL = '';
 export default class Login extends Component {
     static navigationOptions = {
         title: 'Login',
         header: null,
-        
+
     }
     constructor(props) {
         super(props)
@@ -18,94 +18,85 @@ export default class Login extends Component {
             senha: '',
         }
     }
- 
-autenticar_Usuario = () =>
-    {
-    if (this.state.email == '' || this.state.senha == '') {
-        Alert.alert('usuário ou senha incorretos');
-    } else {
-       
-    
-        fetch('https://coworkingsegunda.000webhostapp.com/appVerificaUsuario.php',
-        {
-            method: 'POST',
-            headers: 
-            {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(
-            {
-                email: this.state.email,
-                senha: this.state.senha  
-            })
-        }).then((response) => response.json())
-        .then((responseJson) => {
 
-        if(responseJson == 'Pai')
-        {//parente
-            global.EMAIL = this.state.email;
-            global.TYPE_USER = 0;
-            this.props.navigation.navigate('Dashboard');
-            
-            
-        }
-        else if(responseJson == 'Monitor')
-        {   //monitor
-            global.TYPE_USER = 1;
-            this.props.navigation.navigate('Dashboard');
-            
-            
-        }
-        else 
-            {
-                Alert.alert ("Email ou Senha Invalidos");
-            }
-            
-        }).catch((error) =>
-        {
-            console.error(error);
+    autenticar_Usuario = () => {
+        if (this.state.email == '' || this.state.senha == '') {
+            Alert.alert('usuário ou senha incorretos');
+        } else {
 
-            this.setState({ isLoading : false});
-        });
+
+            fetch('https://coworkingsegunda.000webhostapp.com/appVerificaUsuario.php',
+                {
+                    method: 'POST',
+                    headers:
+                    {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(
+                        {
+                            email: this.state.email,
+                            senha: this.state.senha
+                        })
+                }).then((response) => response.json())
+                .then((responseJson) => {
+
+                    if (responseJson == 'Pai') {//parente
+                        global.EMAIL = this.state.email;
+                        global.TYPE_USER = 0;
+                        this.props.navigation.navigate('Dashboard');
+
+
+                    }
+                    else if (responseJson == 'Monitor') {   //monitor
+                        global.TYPE_USER = 1;
+                        this.props.navigation.navigate('Dashboard');
+
+
+                    }
+                    else {
+                        Alert.alert("Email ou Senha Invalidos");
+                    }
+
+                }).catch((error) => {
+                    console.error(error);
+
+                    this.setState({ isLoading: false });
+                });
+        }
     }
-}
 
     render() {
-        const {navigate} = this.props.navigation;
+        const { navigate } = this.props.navigation;
         return (
-            <KeyboardAvoidingView  behavior={'padding'} style={styles.container}>
+            <KeyboardAvoidingView behavior={'padding'} style={styles.container}>
                 <View style={styles.logoContainer}>
                     <Image
-                    style={styles.logo}
-                     source={require('../assets/provisorioLogo.png')}
-                     />
-                     <Text style={styles.title}> Agenda Aluno</Text>
+                        style={styles.logo}
+                        source={require('../assets/provisorioLogo.png')}
+                    />
+                    <Text style={styles.title}> Agenda Aluno</Text>
                 </View>
                 <View style={styles.containerInputs}>
                     <StatusBar
-                    barStyle='light-content'/>
-                    
-					<TextInput placeholder='Email' 
-					style={styles.TextInput}
-					onChangeText={email => this.setState({email})}/>
-          
-					<TextInput placeholder='Senha' 
-						style={styles.TextInput} 
-						secureTextEntry
-						onChangeText={senha => this.setState({senha})}
-					/>
-                
-                    <View style={styles.buttonContainer}>
-                     <TouchableOpacity 
-						activeOpacity = { 0.5 } 
-						style = { styles.TouchableOpacityStyle } 
-						onPress = { this.autenticar_Usuario }>
+                        barStyle='light-content' />
 
-                    <Text style = { styles.TextStyle }>Acessar</Text>
+                    <TextInput style={styles.TextInput} placeholder='Email'
+                        style={styles.TextInput}
+                        onChangeText={email => this.setState({ email })} />
 
-                </TouchableOpacity>
-                  </View>
+                    <TextInput placeholder='Senha'
+                        style={styles.TextInput}
+                        secureTextEntry
+                        onChangeText={senha => this.setState({ senha })}
+                    />
+
+                        <TouchableOpacity
+                            activeOpacity={0.5}
+                            style={styles.TouchableOpacityStyle}
+                            onPress={this.autenticar_Usuario}>
+                            <Text style={styles.TextStyle}>Acessar</Text>
+                        </TouchableOpacity>
                 </View>
             </KeyboardAvoidingView>
         );
@@ -117,45 +108,42 @@ const styles = StyleSheet.create({
         backgroundColor: '#6ebcbc',
         justifyContent: 'center',
     },
-	
-	TouchableOpacityStyle:
-   {
-      paddingTop:10,
-      paddingBottom:10,
-      backgroundColor:'#009688',
-      marginBottom: 20,
-      width: '100%',
-      textAlign: 'left'
- 
+    TouchableOpacityStyle:
+    {
+        paddingTop: 10,
+        paddingBottom: 10,
+        backgroundColor: '#009688',
+        marginBottom: 20,
+        width: '100%',
+        textAlign: 'left',
     },
-	
     logoContainer: {
         alignItems: 'center',
         flexGrow: 1,
-        justifyContent:'center',
+        justifyContent: 'center',
     },
     logo: {
         width: 200,
         height: 200,
     },
     title: {
-        color:'#f5f6fa',
+        color: '#f5f6fa',
         marginTop: 10,
         textAlign: 'center',
         fontSize: 30,
-        justifyContent:'center',
+        justifyContent: 'center',
     },
     formContainer: {
-        justifyContent:'center',
+        justifyContent: 'center',
     },
     buttonContainer: {
-    backgroundColor: '#3b8181',
-    paddingVertical: 10,
-    textAlign: 'center',
-    color: '#f5f6fa',
-    fontWeight: '700',
+        backgroundColor: '#3b8181',
+        paddingVertical: 10,
+        textAlign: 'center',
+        color: '#f5f6fa',
+        fontWeight: '700',
     },
-    input: {
+    TextInput: {
         height: 40,
         backgroundColor: '#a8d7d7',
         marginBottom: 20,
@@ -164,6 +152,10 @@ const styles = StyleSheet.create({
     },
     containerInputs: {
         padding: 20,
-        justifyContent:'center',
+        justifyContent: 'center',
     },
+    TextStyle: {
+        textAlign: 'center',
+        color: '#ffffff'
+    }
 });
