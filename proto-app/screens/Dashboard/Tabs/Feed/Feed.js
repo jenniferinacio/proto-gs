9d02f321199584469b8c4dcb41b281f585238942
jenniferinacio <jenniferp.i8@gmail.com>
2019-08-13 10:47:01 -0300

    Feed certo

--- proto-app/screens/Dashboard/Tabs/Feed/Feed.js
+++ proto-app/screens/Dashboard/Tabs/Feed/Feed.js
@@ -1,148 +1,127 @@
+/*React Native TimeLine ListView*/
 import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
+import Timeline from 'react-native-timeline-listview';
+import Icon from 'react-native-vector-icons/Ionicons';
+import cadastro from '../../../Login'
 
-import { StyleSheet, View, TextInput, Text, ActivityIndicator, TouchableOpacity, Alert, Picker, Button } from 'react-native';
-
-export default class App extends Component<{}>
-{
-  constructor() {
-    super();
-
-    this.state = {
-      Descricao: '',
-      Id_Turma: '',
-      Id_Responsavel: '',
-      ActivityIndicator_Loading: false,
-    }
+export default class BasicTimeLine extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true };
+    this.data = [{}];
   }
-  Insert_Data_Into_MySQL = () => {
-    this.setState({ ActivityIndicator_Loading: true }, () => {
-      fetch('https://coworkingsegunda.000webhostapp.com/appCadRecado.php',
-        {
-          method: 'POST',
-          headers:
-          {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(
-            {
-              Descricao: this.state.Descricao,
-              Id_Responsavel: this.state.Id_Responsavel,
-              Id_Turma: this.state.Id_Turma
-            })
-        }).then((response) => response.json()).then((responseJsonFromServer) => {
-          alert(responseJsonFromServer);
-          this.setState({ ActivityIndicator_Loading: false });
-        }).catch((error) => {
-          console.error(error);
-          this.setState({ ActivityIndicator_Loading: false });
+  componentDidMount() {
+    return fetch('https://coworkingsegunda.000webhostapp.com/appListAtividade.php')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.data = responseJson
+        this.setState({
+          isLoading: false
+        }, function () {
         });
-    });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
   render() {
+
+    // botões apareces se 'monitor'
+    let cadButtons;
+    if (global.TYPE_USER) {
+
+      cadButtons = <View style={styles.buttonsArea}>
+        <TouchableOpacity style={styles.btns}
+          onPress={() => {
+            this.props.navigation.navigate("screenCadAtividade")
+          }}><Text style={styles.textStyle}>Cadastrar Atividade</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.btns}
+          onPress={() => {
+            this.props.navigation.navigate("screenCadOcorrencia")
+          }}><Text style={styles.textStyle}>Cadastrar Ocorrência</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.bt}
+          onPress={() => {
+            this.props.navigation.navigate("screenCadRecado")
+          }}><Text style={styles.textStyle}>Cadastrar Recado</Text></TouchableOpacity>
+      </View>
+
+    } else {
+      cadButtons = null;
+    }
+
     return (
-      <View style={styles.MainContainer}>
+      <View style={styles.container}>
+        <Text
+          style={{
+            padding: 16,
+            fontSize: 20,
+            textAlign: 'center',
+            fontWeight: 'bold',
+          }}>
+          Linha do tempo de atividades
+        </Text>
 
-        <Text style={styles.TextStyleClass} >Cadastro de Recados</Text>
-        <Text> </Text>
-        <Text> </Text>
-
-        <TextInput
-          placeholder="ID. Responsável"
-          style={styles.TextInputStyleClass}
-          underlineColorAndroid="transparent"
-          keyboardType='numeric'
-          onChangeText={(TextInputText) => this.setState({ Id_Responsavel: TextInputText })} />
-
-        <TextInput
-          placeholder="No. Turma"
-          style={styles.TextInputStyleClass}
-          underlineColorAndroid="transparent"
-          keyboardType='numeric'
-          onChangeText={(TextInputText) => this.setState({ Id_Turma: TextInputText })} />
-
-        <TextInput
-          placeholder="Descrição do Recado"
-          style={styles.TextInputStyleClass}
-          underlineColorAndroid="transparent"
-          onChangeText={(TextInputText) => this.setState({ Descricao: TextInputText })} />
-
-        <Text> </Text>
-        <TouchableOpacity
-          activeOpacity={0.5}
-          style={styles.TouchableOpacityStyle}
-          onPress={this.Insert_Data_Into_MySQL}>
-          <Text style={styles.TextStyle}>Cadastrar Recado</Text>
-        </TouchableOpacity>
-        {
-          this.state.ActivityIndicator_Loading ? <ActivityIndicator color='#009688' size='large' style={styles.ActivityIndicatorStyle} /> : null
-        }
+        {cadButtons}
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+        <Timeline style={styles.list} data={this.data} circleSize={20} circleColor='#f98b9c' lineColor='#f19e9e' timeStyle={{
+          textAlign: 'center',
+          backgroundColor: '#6ebcbc',
+          color: 'white',
+          padding: 5,
+          borderRadius: 13
+        }} descriptionStyle={{
+          color: 'gray'
+        }} options={{
+          style: {
+            paddingTop: 5
+          }
+        }} innerCircle={'dot'} onEventPress={this.onEventPress} separator={false} detailContainerStyle={{
+          marginBottom: 20,
+          paddingLeft: 5,
+          paddingRight: 5,
+          backgroundColor: "#def9ff",
+          borderRadius: 10
+        }} columnFormat='two-column' />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create(
-  {
-    MainContainer:
-    {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: 20,
-      backgroundColor: '#fff'
-    },
-
-    TextStyleClass:
-    {
-
-      textAlign: 'center',
-      height: 40,
-      backgroundColor: "#fff",
-      marginBottom: 10,
-      width: '95%',
-      color: '#009688',
-      fontSize: 20
-    },
-    TextInputStyleClass:
-    {
-
-      textAlign: 'center',
-      height: 40,
-      backgroundColor: "#fff",
-      borderWidth: 1,
-      borderColor: '#009688',
-      borderRadius: 7,
-      marginBottom: 10,
-      width: '95%'
-    },
-
-    TouchableOpacityStyle:
-    {
-      paddingTop: 10,
-      paddingBottom: 10,
-      backgroundColor: '#009688',
-      marginBottom: 20,
-      width: '90%'
-
-    },
-
-    TextStyle:
-    {
-      color: '#fff',
-      textAlign: 'center',
-      fontSize: 18
-    },
-
-    ActivityIndicatorStyle: {
-
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-      alignItems: 'center',
-      justifyContent: 'center'
-
-    }
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'white',
+  },
+  buttonsArea: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 0.2,
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+  btns: {
+    alignItems: 'center',
+    backgroundColor: '#009688',
+    flexDirection: 'row',
+    color: '#ffff',
+    width: '45%',
+    height: 40
+  },
+  bt: {
+  
+    backgroundColor: '#009688',
+    color: '#ffff',
+    flexDirection: 'column',
+    width: '45%',
+    height: 40
+  },
+  textStyle: {
+    textAlign: 'center',
+    color: '#fff'
+  }
+});
